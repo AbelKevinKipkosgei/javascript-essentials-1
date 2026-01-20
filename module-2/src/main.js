8b14bd35163edc0b7c329bcfc7c5fafa5c3e7955
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2026-01-20 04:42:24 +0300

    Add JavaScript code to illustrate variables

---
+++ module-2/src/main.js
@@ -0,0 +1,12 @@
+// variables
+let greeting = "Hello";
+let counter = 100;
+
+console.log(greeting);
+console.log(counter);
+
+greeting = "Hello, Abel Kevin";
+counter = counter + 120;
+
+console.log("Modified greeting: " + greeting);
+console.log("Modified counter: " + counter);
