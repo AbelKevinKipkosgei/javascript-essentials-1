02a68c9036481dff9c7e2c6bbc90464707e12d2d
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-01-23 16:01:10 +0300

    Display person1 and person3 contact details

---
+++ module-2/lab/src/lab.js
@@ -0,0 +1,17 @@
+//names
+const person1 = "Maxwell Wright";
+const person2 = "Raja Villarreal";
+const person3 = "Helen Richards";
+
+//phone numbers
+const phone1 = "(0191) 719 6495";
+const phone2 = "0866 398 2895";
+const phone3 = "0800 1111";
+
+//emails
+const email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+const email2 = "posuere.vulputate@sed.com";
+const email3 = "libero@convallis.edu";
+
+console.log(`${person1}/${phone1}/"${email1}`)
+console.log(`${person3}/${phone3}/${email3}`)
