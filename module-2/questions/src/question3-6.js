b685a334543ac5883b3a50c2997c2bf0ac626506
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2026-01-24 14:11:01 +0300

    Update JS code to solve question 3 to 6

---
+++ module-2/questions/src/question3-6.js
@@ -0,0 +1,16 @@
+//chain conversion
+const str1 = "1234";
+console.log(str1);
+
+const num1 = Number(str1);
+console.log(num1);
+
+const big1 = BigInt(num1);
+console.log(big1);
+
+const b1 = Boolean(big1);
+console.log(b1);
+
+//get 43
+const str2 = 42 + Number("1");
+console.log(str2);
