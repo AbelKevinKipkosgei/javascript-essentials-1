2ba3e19290ae0701dcdc44ae6c28336d74c8a28a
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2026-01-24 13:57:30 +0300

    Write JavaScript code to solve question 3

---
+++ module-2/questions/src/question3.js
@@ -0,0 +1,12 @@
+//chain conversion
+const str1 = "1234";
+console.log(str1);
+
+const num1 = Number(str1);
+console.log(num1);
+
+const big1 = BigInt(num1);
+console.log(big1);
+
+const b1 = Boolean(big1);
+console.log(b1);
