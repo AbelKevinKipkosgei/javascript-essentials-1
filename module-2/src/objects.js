98a2e861d137ab81252d4acad488774ab32396d1
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2026-01-24 17:06:23 +0300

    Add code to illustrate objects in JavaScript

---
+++ module-2/src/objects.js
@@ -0,0 +1,18 @@
+//objects
+
+let user1 = {
+  name: "Calvin",
+  surname: "Hart",
+  age: 66,
+  email: "CalvinMHart@teleworm.us",
+};
+
+let user2 = {
+  name: "Mateus",
+  surname: "Pinto",
+  age: 21,
+  email: "MateusPinto@dayrep.com",
+};
+
+console.log(user1.name);
+console.log(user2.name);
