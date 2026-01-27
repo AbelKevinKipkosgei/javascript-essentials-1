28bd374523b80603b3a725f304a071a20557ad83
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-01-27 16:22:00 +0300

    Add three variables for user input via prompt

---
+++ module-4/src/main1.js
@@ -0,0 +1,4 @@
+// calculator program
+const firstNumber = prompt("Enter first number: ", 10);
+const operator = prompt("Enter an operator: (+, -, *, /)", "*");
+const secondNumber = prompt("Enter second number: ", 12);
