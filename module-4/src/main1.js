573cf9349e40c8f028f24d03261b0be3e1b1b49d
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-01-28 10:33:28 +0300

    Update switch statements and type checking

--- module-4/src/main1.js
+++ module-4/src/main1.js
@@ -3,17 +3,28 @@ const firstNumber = Number(prompt("Enter first number: ", 10));
 const secondNumber = Number(prompt("Enter second number: ", 12));
 const operator = prompt("Enter an operator: (+, -, *, /)", "*");
 
-switch (operator) {
-  case "+":
-    const sum = firstNumber + secondNumber;
-    alert(`Sum: ${sum}`);
-    break;
-  case "-":
-    const difference = firstNumber + secondNumber;
-    alert(`Difference: ${difference}`);
-    break;
-  case "*":
-    const multiplication = firstNumber * secondNumber;
-    alert(`Multiplication: ${multiplication}`);
-    break;
+// checking for valid numbers
+if (Number.isFinite(firstNumber) && Number.isFinite(secondNumber)) {
+  switch (operator) {
+    case "+":
+      const sum = firstNumber + secondNumber;
+      alert(`Sum: ${sum}`);
+      break;
+    case "-":
+      const difference = firstNumber - secondNumber;
+      alert(`Difference: ${difference}`);
+      break;
+    case "*":
+      const multiplication = firstNumber * secondNumber;
+      alert(`Multiplication: ${multiplication}`);
+      break;
+    case "/":
+      const division = firstNumber / secondNumber;
+      alert(`Division: ${division}`);
+      break;
+    default:
+      alert("Invalid operation");
+  }
+} else {
+  alert("Invalid input!! Use only numbers.");
 }
