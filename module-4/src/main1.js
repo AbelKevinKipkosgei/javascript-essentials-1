623448cf82dc3957d44d985c817e1b5b7577de37
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-01-28 09:10:38 +0300

    Setup switch case for calculator

--- module-4/src/main1.js
+++ module-4/src/main1.js
@@ -1,4 +1,19 @@
 // calculator program
-const firstNumber = prompt("Enter first number: ", 10);
+const firstNumber = Number(prompt("Enter first number: ", 10));
+const secondNumber = Number(prompt("Enter second number: ", 12));
 const operator = prompt("Enter an operator: (+, -, *, /)", "*");
-const secondNumber = prompt("Enter second number: ", 12);
+
+switch (operator) {
+  case "+":
+    const sum = firstNumber + secondNumber;
+    alert(`Sum: ${sum}`);
+    break;
+  case "-":
+    const difference = firstNumber + secondNumber;
+    alert(`Difference: ${difference}`);
+    break;
+  case "*":
+    const multiplication = firstNumber * secondNumber;
+    alert(`Multiplication: ${multiplication}`);
+    break;
+}
